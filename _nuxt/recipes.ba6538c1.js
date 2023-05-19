522f51c1c45c0fd8a960d0b0eefb38d111cbb196
dubietbay <84702597+LandoftheRisingSun@users.noreply.github.com>
2023-05-19 23:05:08 +0700

    Updates

---
+++ _nuxt/recipes.ba6538c1.js
@@ -0,0 +1,1 @@
+import{p as e,j as c,o as r}from"./entry.4642d0f8.js";const a={};function n(o,t){return r(),c("div",null," awdawdawd ")}const _=e(a,[["render",n]]);export{_ as default};
--- about/_payload.js
+++
@@ -1,1 +0,0 @@
-export default {data:{},prerenderedAt:1684504385159}
